a16bd25ae59392063d58756e51daea97ab6e5852
kosude <jackbennett0708@gmail.com>
2023-01-08 13:52:30 +0000

    Deploying to gh-pages from @ kosude/thallium@64df7b7575d73a06f26c47cf1582022b2e3e575c 🚀

--- de/d8c/a00036.js
+++ de/d8c/a00036.js
@@ -14,7 +14,8 @@ var a00036 =
       [ "layerNames", "df/d03/a00062.html#a26b1143a0b414f1e3f48199116e2f911", null ],
       [ "layerCount", "df/d03/a00062.html#a7a400a317a41183992ef5ad93c83b5f3", null ],
       [ "debugMessengerSeverities", "df/d03/a00062.html#ae98c5e288efe7d7cc7f025368ce241b6", null ],
-      [ "debugMessengerTypes", "df/d03/a00062.html#ac1a05c059276bf0955bed10ce7699bdc", null ]
+      [ "debugMessengerTypes", "df/d03/a00062.html#ac1a05c059276bf0955bed10ce7699bdc", null ],
+      [ "detailedDebugMessenger", "df/d03/a00062.html#a127dd54eb70145545cf40b5d35982e75", null ]
     ] ],
     [ "thvk_InitRenderSystem", "de/d8c/a00036.html#ga1c687d01c097bb52daeff667bc1dacdf", null ]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['debugmessengerseverities_0',['debugMessengerSeverities',['../df/d03/a00062.html#ae98c5e288efe7d7cc7f025368ce241b6',1,'thvk_RenderSystemDescriptor_t']]],
-  ['debugmessengertypes_1',['debugMessengerTypes',['../df/d03/a00062.html#ac1a05c059276bf0955bed10ce7699bdc',1,'thvk_RenderSystemDescriptor_t']]]
+  ['debugmessengertypes_1',['debugMessengerTypes',['../df/d03/a00062.html#ac1a05c059276bf0955bed10ce7699bdc',1,'thvk_RenderSystemDescriptor_t']]],
+  ['detaileddebugmessenger_2',['detailedDebugMessenger',['../df/d03/a00062.html#a127dd54eb70145545cf40b5d35982e75',1,'thvk_RenderSystemDescriptor_t']]]
 ];
