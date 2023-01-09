1c82d7fccb6a2314ed3ca637dc3764e9757d6335
Jack Bennett <jackbennett0708@gmail.com>
2023-01-09 20:52:55 +0000

    Allow disabling dynamic version retrieval

---
+++ cmake/generate/dyn_version_skip.h
@@ -0,0 +1,10 @@
+// The project version was not dynamically retrieved so this file has replaced
+// the usual dyn_version.h.
+
+#pragma once
+#ifndef _DYN_VERSION_H
+#define _DYN_VERSION_H
+
+#define THALLIUM_VERSION "unknown_version"
+
+#endif // _DYN_VERSION_H
