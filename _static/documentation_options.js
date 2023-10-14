b9442ace8bc4118eeb133fe54d3f043407eec3fe
kosude <jackbennett0708@gmail.com>
2023-10-14 20:39:42 +0000

    Deploying to gh-pages from @ kosude/thallium@145d86817d6d8124041ae59316457c7bb3ebc9a4 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.0dev_1e43d24 (2023-10-14)',
+    VERSION: 'v0.0.0dev_145d868 (2023-10-14)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
